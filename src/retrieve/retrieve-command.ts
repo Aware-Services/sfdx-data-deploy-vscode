ae8b70089fb036104eb73086baef831e85981580
mattbathersby <68982877+mattbathersby@users.noreply.github.com>
2021-02-24 12:49:24 +1100

    added config.target support

--- src/retrieve/retrieve-command.ts
+++ src/retrieve/retrieve-command.ts
@@ -7,33 +7,35 @@ export default async function retrieveCommand(
   deployDir?: string,
   include?: string[]
 ): Promise<void> {
-  const deployDirectory = deployDir || (await selectDeployDirectory())?.detail;
-  if (!deployDirectory) {
-    return;
+      const deployDirectory = deployDir || ((_a = (yield select_deploy_directory_1.default())) === null || _a === void 0 ? void 0 : _a.detail);
+      if (!deployDirectory) {
+          return;
+      }
+  
+      let descriptor;
+      let target;
+  
+      try {
+          descriptor = yield read_descriptor_1.default(deployDirectory);
+          target = descriptor.targets.retrieve;
+      }
+      catch (error) {
+          vscode_1.window.showErrorMessage(error.message);
+          return;
+      }
+      
+      const includeFiles = include || (yield select_data_files_1.default(descriptor));
+      
+      if (!includeFiles) {
+          return;
+      }
+      const terminal = vscode_1.window.createTerminal('SFDX Data Retrieve');
+      terminal.show(true);
+      if (includeFiles.length > 0) {
+          const includeParam = includeFiles.join(',');
+          terminal.sendText(`sfdx datadeploy:retrieve -d "${deployDirectory}" -i "${includeParam}" -u "${target}"`);
+      }
+      else {
+          terminal.sendText(`sfdx datadeploy:retrieve -d "${deployDirectory}" -u "${target}"`);
+      }
   }
-
-  let descriptor;
-  try {
-    descriptor = await readDescriptor(deployDirectory);
-  } catch (error) {
-    window.showErrorMessage(error.message);
-    return;
-  }
-
-  const includeFiles = include || (await selectDataFiles(descriptor));
-  if (!includeFiles) {
-    return;
-  }
-
-  const terminal = window.createTerminal('SFDX Data Retrieve');
-  terminal.show(true);
-
-  if (includeFiles.length > 0) {
-    const includeParam = includeFiles.join(',');
-    terminal.sendText(
-      `sfdx datadeploy:retrieve -d "${deployDirectory}" -i "${includeParam}"`
-    );
-  } else {
-    terminal.sendText(`sfdx datadeploy:retrieve -d "${deployDirectory}"`);
-  }
-}
